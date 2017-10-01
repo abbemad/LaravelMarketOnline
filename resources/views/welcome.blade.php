f996e79d6e38b3e102025b4eab4e54b2cb858982
Damien <abbemad@gmail.com>
2017-10-01 23:20:26 +0200

    Created links for the pages in welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url ('/shop') }}">Products</a>
+                    <a href="{{ url ('/about') }}">About</a>
+                    <a href="{{ url ('/contact') }}">Contact</a>
+                    
                 </div>
             </div>
         </div>
