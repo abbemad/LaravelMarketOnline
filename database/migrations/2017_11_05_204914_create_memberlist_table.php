bd8e51eaf95342c6608282ae5959e53ab10e4d9d
Damien <abbemad@gmail.com>
2017-11-05 22:27:08 +0100

    added member db

---
+++ database/migrations/2017_11_05_204914_create_memberlist_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberlistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memberlist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->foreign('member_id')->reference('id')->on('member');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memberlist');
+    }
+}
