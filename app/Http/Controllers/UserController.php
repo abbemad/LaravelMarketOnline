ffe8db43d69d4527cc246a7a72ca2aa5e10eaeef
Damien <abbemad@gmail.com>
2018-01-16 05:12:42 +0100

    Redirect with auth for non admins

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,22 +8,11 @@ use App\User;
 
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    
+   
     public function __construct()
     {
         $this->middleware('auth:admin');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
     public function index(){
 
