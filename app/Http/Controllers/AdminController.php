3a72ccaeb53df1df01bbdaab70cd1712ffd427a1
Damien <abbemad@gmail.com>
2018-01-16 05:24:13 +0100

    Notes

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -28,3 +28,6 @@ class AdminController extends Controller
         return view('admin');
     }
 }
+
+// Proberen admin controller te extenden met UserController zodat niet constant
+// de functie aangeroepen moet worden in de andere controllers maar hij deze extend
